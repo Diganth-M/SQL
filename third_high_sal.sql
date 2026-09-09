3d814f6bb318a902653e58bbb0c81a32117ca36f
Diganth M <diganthhema5712@gmail.com>
2026-09-09 17:38:21 +0530

    Implement query for third highest salary
    Added a new approach to retrieve the third highest salary.

--- third_high_sal.sql
+++ third_high_sal.sql
@@ -4,3 +4,11 @@ Select name,max(salary)
   from Employee
   where salary<(select max(salary) from Employee where salary< (Select max(salary)
   from Employee));
+
+
+-- approach
+
+Select name,salary
+  from Employee
+  order by salary desc
+  limit 1 offset 2;
