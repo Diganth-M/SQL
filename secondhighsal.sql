6302a53d8c22a432403ed09cccb81aee0a7abbf5
Diganth M <diganthhema5712@gmail.com>
2026-09-09 17:32:05 +0530

    Add second approach for second highest salary query
    Added a second approach to find the second highest salary.

--- secondhighsal.sql
+++ secondhighsal.sql
@@ -2,3 +2,11 @@
 Select name,max(salary)
   from Employee
   where salary<(select max(salary) from Employee);
+
+
+-- approac 2
+
+Select name,salary
+  from Employee
+  order by salary desc
+  limit 1 offset 1;
